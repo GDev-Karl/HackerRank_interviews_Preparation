f4011dcddc8c8f95323c143426fd66fb5ef2786b
Karl <tekamkarl@gmail.com>
2024-06-18 09:34:17 +0100

    Calculate the ratios of positive, negative and zero values in the given array

--- PlusMinus/PlusMinus.py
+++ PlusMinus/PlusMinus.py
@@ -1,12 +1,5 @@
 #!/bin/python3
 
-import math
-import os
-import random
-import re
-import sys
-
-
 def plusMinus(arr):
     """
     Calculate the ratios of positive, negative and zero values in the given array.
